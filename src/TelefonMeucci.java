0a6dca98200063e0af4519f95f02a4a449a169df
Aaron <aaronaguirre@inspladelestany.cat>
2022-02-07 17:05:26 +0100

    Initial commit

---
+++ src/TelefonMeucci.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TelefonMeucci {
+    static int contador=0;
+    static String fr="";
+    static String[] set1 = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+    public static void main(String[] args) {
+
+        Scanner t = new Scanner(System.in);
+        while(t.hasNext()){
+            contador = 0;
+            fr=t.next();
+            if(!fr.contains("I")&&!fr.contains("V"))
+                contador++;
+            else
+                comprova(fr);
+            System.out.println(contador);
+        }
+    }
+
+    private static void comprova(String f) {
+        for(int i=1;i<5&&i<=f.length();i++){
+            boolean trobat=false;
+            for(int j=0;j<10;j++){
+                if(f.substring(0,i).equals(set1[j]))
+                    trobat=true;
+            }
+            if(trobat){
+                String res=f.substring(i,f.length());
+                if(res.length()>0)
+                    comprova(res);
+                else if(res.length()==0){
+                    contador++;
+                    return;
+                }
+            }
+        }
+    }
+}
+
